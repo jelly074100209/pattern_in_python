243678fe08a5ac755c9902e765ca486111fd91aa
jelly074100209 <419756656@qq.com>
2016-12-19 11:16:56 +0800

    jss

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # main file
 
 print("Hello World")
+
+
+class Singleton(object):
+    def __new__(cls, *args, **kw):
+        if not hasattr(cls, '_instance'):
+            origin = super(Singleton, cls)
+            cls._instance = origin.__new__(cls, *args, **kw)
+        return cls._instance
+
+
+class MyClass(Singleton):
+    a = 1
+
+
+one = MyClass()
+two = MyClass()
+
+two.a = 3
+print(one.a)
